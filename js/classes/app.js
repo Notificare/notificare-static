b72f3971d3cd8327eb1be2b8fe174bde0643c1e4
jjonthemix <joeloliveira1979@gmail.com>
2014-08-05 02:40:10 +0200

    changed plans accordingly, added signup popup form

--- js/classes/app.js
+++ js/classes/app.js
@@ -123,8 +123,87 @@ $( document ).ready(function() {
 //	}, 500);
 	
 	
-	$('.carousel').carousel({
-		interval: false
+	$('.btn-order').click(function(e) {
+		$('#modal-signup').modal('show');
+		var plan = $(this).attr("rel"),
+        href = $(this).attr("href");
+		url_components = href.split("/"),
+		planname = url_components[url_components.length - 1],
+		$('.modal-title').html('Your choice: ' + plan);
+        var $form = $( '#signup' );
+        $form.find( "input[name='plan']" ).val(planname);
+		return false;
 	});
-	
+
+
+    $('.currency-change').change(function(e) {
+        if($(this).val() == 'EUR'){
+            $('.dollar').hide();
+            $('.euro').show();
+        } else {
+            $('.euro').hide();
+            $('.dollar').show();
+        }
+    });
+
+    $('.signup-form-button').click(function(e) {
+        e.preventDefault();
+        // Get some values from elements on the page:
+        var $form = $( '#signup' ),
+            firstName = $form.find( "input[name='firstname']" ).val(),
+            lastName = $form.find( "input[name='lastname']" ).val(),
+            country = $form.find( "select[name='country']" ).val(),
+            company = $form.find( "input[name='company']" ).val(),
+            telephone = $form.find( "input[name='telephone']" ).val(),
+            pass1 = $form.find( "input[name='pass1']" ).val(),
+            pass2 = $form.find( "input[name='pass2']" ).val(),
+            email = $form.find( "input[name='email']" ).val(),
+            plan = $form.find( "input[name='plan']" ).val(),
+            url = $form.attr( "action");
+
+        console.log({
+            name: firstName + ' ' + lastName,
+            email: email,
+            password: pass1,
+            plan: plan,
+            billing_info: {
+                company: company,
+                first_name: firstName,
+                last_name: lastName,
+                country: country,
+                phone: telephone
+            },
+            preferences:{
+                currency: $('.currency-change').val()
+            }
+        });
+//
+//        var request = $.ajax({
+//            url: url,
+//            type: "POST",
+//            data: {
+//                name: firstName + ' ' + lastName,
+//                email: email,
+//                password: password,
+//                plan: plan,
+//                billing_info: {
+//                    company: company,
+//                    first_name: firstName,
+//                    last_name: lastName,
+//                    country: country,
+//                    phone: telephone
+//                }
+//            },
+//            dataType: "json"
+//        });
+//
+//        request.done(function( msg ) {
+//            $( ".signup-results" ).html( msg );
+//        });
+//
+//        request.fail(function( jqXHR, textStatus ) {
+//            $( ".signup-results" ).html( textStatus );
+//        });
+
+    });
 });
